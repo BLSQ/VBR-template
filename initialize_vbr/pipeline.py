25b8e89fca88c3efba8b924b5927bf94c6a77932
jhubinon <jhubinon@gmail.com>
2025-04-09 18:18:58 +0200

    fix(init vbr): rename period to month or quarter

--- initialize_vbr/pipeline.py
+++ initialize_vbr/pipeline.py
@@ -8,12 +8,7 @@ from RBV_package import rbv_environment
 from openhexa.toolbox.dhis2 import DHIS2
 
 
-@pipeline(
-    "initialize_vbr",
-    name="Initialize VBR",
-    description="Read Hesabu configuration, extract\
-          and DHIS2 data, and initiate a class oriented model for simulations.",
-)
+@pipeline("initialize_vbr")
 @parameter("dhis_con", type=DHIS2Connection, help="Connection to DHIS2", required=True)
 @parameter("hesabu_con", type=str, help="Connection to hesabu", default="hesabu", required=True)
 @parameter(
@@ -85,8 +80,9 @@ def prepare_quantity_data(done, periods, packages, contracts, hesabu_params, ext
                     if os.path.exists(f"{workspace.files_path}/packages/{id}/{p}.csv"):
                         df = pd.read_csv(f"{workspace.files_path}/packages/{id}/{p}.csv")
                         data = pd.concat([data, df], ignore_index=True)
+        data = data.rename(columns={"period": "month"})
         data = data.merge(contracts, on="org_unit_id")
-        data = data[(data.contract_end_date >= data.month) & (~data.type_ou.isnull())]
+        data = data[(data.contract_end_date >= data.month) & (~data.type_ou.isna())]
         data = data[
             list(hesabu_params["quantite_attributes"].keys())
             + list(hesabu_params["contracts_attributes"].keys())
@@ -123,6 +119,7 @@ def prepare_quality_data(done, periods, packages, hesabu_params, extract):
                     if os.path.exists(f"{workspace.files_path}/packages/{id}/{p}.csv"):
                         df = pd.read_csv(f"{workspace.files_path}/packages/{id}/{p}.csv")
                         data = pd.concat([data, df], ignore_index=True)
+        data = data.rename(columns={"period": "quarter"})
         data = data[list(hesabu_params["qualite_attributes"].keys())]
         data.rename(
             columns=hesabu_params["qualite_attributes"],
