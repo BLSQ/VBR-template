02cefdf7c6d0eb241dab0c6413b478846334ad90
jhubinon <31408172+jhubinon@users.noreply.github.com>
2025-04-10 10:24:20 +0200

    Update setup.py

--- RBV_package/setup.py
+++ RBV_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rbv_package",
-    version="1.0.36",
+    version="1.0.37",
     packages=find_packages(),
     description="Un package de fonction utiles pour la Vérification basée sur les risques",
     author="Hubinont Jean-Philippe",
