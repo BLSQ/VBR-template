5749b1653006e1322d27ed114351a2de8017c054
lgarridobsq <lgarrido@bluesquarehub.com>
2025-04-24 12:00:58 +0200

    up the version

--- rbv-package/setup.py
+++ rbv-package/setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rbv-package",
-    version="1.0.40",
-
+    version="1.0.41",
     packages=find_packages(),
     description="Un package de fonction utiles pour la Vérification basée sur les risques",
     author="Hubinont Jean-Philippe",
