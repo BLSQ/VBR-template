1bd49b5af38a8783e3695b2ef57d44785a29bece
lgarridobsq <lgarrido@bluesquarehub.com>
2026-04-21 17:42:51 +0200

    initialize pipeline

---
+++ country_pipelines/burundi/push_ous/config.py
@@ -0,0 +1,14 @@
+OU_OPENING_DATE = "1970-01-01"
+SHORT_NAME_MAX_LENGTH = 50
+GASC_NAME_PREFIX = "GASC - "
+
+level_5_changes = {
+    "Kirundo": [["CDS nyakarama", "CDS Nyakarama"], ["CDS Marembo Adventiste", "CDS Marembo"]],
+    "Ngozi": [["CDS Taba 1", "CDS Taba I"]],
+    # "Bubanza": [["CDS Rugazi", "HC Rugazi"]],
+    "Muyinga": [["CDS Bonero 2", "CDS Bonero II"]],
+    "Makamba": [["CDS Murekerezi", "CDS Mukerezi"], ["CDS Gatabo", "CDS Gatabo Privé"]],
+    "Cankuzo": [["CDS Musenyi", "CDS Musenyi (de Cankuzo)"]],
+    "Cibitoke": [["CDS Buganda", "CDS Buganda Adventiste"]],
+    "Kayanza": [["CDS Saint Luc de Ndava", "CDS Saint Luc"]],
+}
